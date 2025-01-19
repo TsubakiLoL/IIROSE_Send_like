579db00cb174604926538a6d58349fb15f04d330
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2025-01-20 03:10:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 
+
 //通过UID给用户点赞
 function send_like_by_uid(uid){
 	_alert("点赞UID："+uid);
 	socket.send("+*"+uid);
 
 };
+
+
 //建立绑定到指定uid的点赞按钮
 function add_like_btn_by_uid(uid){
+	console.log("添加点赞按钮"+uid);
 	var selectholderbox = document.getElementById("selectHolderBox");
 	var div=`
 <div id="send_like_div" onmouseenter="Utils.Sound.play(0);" class="selectHolderBoxItem selectHolderBoxItemIcon"><div class="mdi-account-cog" style="font-family:md;font-size:28px;text-align:center;line-height:100px;height:100px;width:100px;position:absolute;top:0;opacity:.7;left:0;"></div>点赞<div id="send_like_touch" class="fullBox whoisTouch3"></div></div>`;
@@ -14,55 +18,63 @@ function add_like_btn_by_uid(uid){
 	var send_like_touch=document.getElementById("send_like_touch");
 	send_like_touch.addEventListener("click", function () {
         send_like_by_uid(uid);
-        _alert("点赞成功！");
     });
 
 }
 
 
-//树更改时进行的回调
-function callback (mutationList, observer) {
-    mutationList.forEach((mutation) => {
-      switch (mutation.type) {
-        case 'childList':
-          /* 从树上添加或移除一个或更多的子节点；参见 mutation.addedNodes 与
-             mutation.removedNodes */
-	         for (let i = 0; i <mutation.addedNodes.length; i++) {
-		          var node=mutation.addedNodes[i];
-		          if(node.classList.contains("msg") && node.hasAttribute("data-id") ){
-			            var data_id=node.getAttribute("data-id");
-  			            let uid=data_id.slice(0,13);
-			            var whoistouccbtn=node.children[0].children[1];
-			            whoistouccbtn.addEventListener("click", function () {
-				              add_like_btn_by_uid(uid);
-			            });
-		          }
-	         }	
-          break
-      }
-    })
-  };
+//打印参数
+function printParams(...param) {
+    for (let i = 0; i < param.length; i++) {
+        console.log(`参数 ${i}:`, param[i]);
+    }
+}
 
-var fullboxholder= document.getElementById("msgholder");
- let observerOptions = {
-    childList: true, // 观察该元素的子元素新增或者删除
-    subtree: true, //该元素的所有子元素新增或者删除
+// 回调函数
+function msgBtnClick(...param) {
+	let type=arguments[0]
+	printParams(...param)
+	console.log("this为：",this)
+	switch(type){
+		case 7:
+   			switch(arguments.length) {
+     				case 1:
+        				console.log("参数长度为1");
+					console.log(this instanceof Element);
+					if(this instanceof Element && this.hasAttribute("uid")){
+						add_like_btn_by_uid(this.getAttribute("uid"));
+					}
+        			break;
+    	 			case 2:
+					console.log("参数长度为2");
+        				let uid=arguments[1][5]
+					add_like_btn_by_uid(uid)
+        			break;
+	        } 
+        break;
+    }
+}
 
-  };
 
-let observer = new MutationObserver(callback);
+// 代理函数
+function proxyFunction(targetFunction, callback) {
+    return function(...param) {
+        console.log("代理函数被调用！参数为", param);
+	let return_v=targetFunction.call(this,...param);
+        // 调用回调函数
+        callback.call(this,...param);
+        return return_v;
+    };
+}
 
-observer.observe(fullboxholder, observerOptions);
+// 缓存 
+var target_function_cache = Objs.mapHolder.function.event;
 
-var msg_all_arr=document.getElementsByClassName("msg");
-for (let i = 0; i <msg_all_arr.length; i++) {
-	if (msg_all_arr[i].hasAttribute("data-id")){
-	    	var node=msg_all_arr[i];
-	   	var data_id=node.getAttribute("data-id");
-		let uid=data_id.slice(0,13);
-		var whoistouccbtn=node.children[0].children[1];
-		whoistouccbtn.addEventListener("click", function () {
-			add_like_btn_by_uid(uid);
-		 });
-	}
- }
+// 绑定代理函数
+Objs.mapHolder.function.event = proxyFunction(
+    target_function_cache, // 原始 Native Code
+    msgBtnClick, // 回调函数
+);
+
+
+console.log(Objs.mapHolder.function.event==target_function_cache);
